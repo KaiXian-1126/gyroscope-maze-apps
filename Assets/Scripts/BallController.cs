6fdd8f4b4429fbc3f186f0b3d21e7c9b0b59f01f
TOK KAI XIAN <kaixianp@gmail.com>
2021-03-30 14:58:40 +0800

    Gyroscope Maze Game Commit

---
+++ Assets/Scripts/BallController.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallController : MonoBehaviour
+{
+    public float ballSpeed;
+    public Rigidbody ballRigidbody;
+    public float xSpeed, ySpeed;
+    Gyroscope m_Gyro;
+
+    //For the end of the game
+    public GameObject gameMenu;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Input.gyro.enabled = true;
+        ballRigidbody = GetComponent<Rigidbody>();
+        m_Gyro = Input.gyro;
+        m_Gyro.enabled = true;
+        ballSpeed = 50;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        xSpeed = -m_Gyro.attitude.x;
+        ySpeed = -m_Gyro.attitude.y;
+        ballRigidbody.AddTorque(new Vector3(xSpeed, ySpeed, 0) * ballSpeed * Time.deltaTime);
+    }
+    void OnCollisionEnter(Collision collision)
+    {
+
+        if (collision.gameObject.name == "Goal")
+        {
+            Destroy(collision.gameObject);
+            //To set the menu active
+            gameMenu.SetActive(true);
+            //To pause the game
+            Time.timeScale = 0;
+        }
+
+    }
+}
---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class UIManager : MonoBehaviour
+{
+    public void startGame()
+    {
+        // The index of the first level is 1
+        SceneManager.LoadScene(1);
+    }
+    public void restartGame() //For the Play Again button
+    {
+        Scene loadedLevel = SceneManager.GetActiveScene();
+        SceneManager.LoadScene(loadedLevel.buildIndex);
+        //Unpaused the game
+        Time.timeScale = 1;
+    }
+    public void goNextLevel() //For the Next Level button
+    {
+        Scene loadedLevel = SceneManager.GetActiveScene();
+        SceneManager.LoadScene(loadedLevel.buildIndex + 1);
+        //Unpaused the game
+        Time.timeScale = 1;
+    }
+    public void quitGame() //For the Exit button
+    {
+        Application.Quit();
+
+    }
+}
